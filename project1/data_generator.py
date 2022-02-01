6a6eb2b609c0affb186a22521912e155ff2f7d13
Jostein Tysse <josteiht@stud.ntnu.no>
2022-02-01 14:51:57 +0100

    Feat cross data generation

--- project1/data_generator.py
+++ project1/data_generator.py
@@ -4,21 +4,60 @@ import numpy as np
 class DataGenerator:
 
     @staticmethod
-    def HorizontalBar(size: int = 16):
+    def HorizontalBar(size: int = 16, center=False):
 
         img = np.zeros((size, size), dtype=np.bool)
 
-        for i in np.random.choice(np.arange(size), np.random.randint(1, int(size/3))):
+        if center:
+            lower_bound = int(size/2) - 3
+            upper_bound = int(size/2) + 4
+        else:
+            lower_bound = 1
+            upper_bound = size - 1
+
+        for i in np.random.choice(np.arange(lower_bound, upper_bound), np.random.randint(1, min(int(size/4), upper_bound-lower_bound - 1))):
             img[i, :] = np.True_
         return img
 
     @staticmethod
-    def VerticalBar(size: int = 16):
+    def VerticalBar(size: int = 16, center=False):
 
-        img = np.zeros((size, size))
+        img = np.zeros((size, size), dtype=np.bool)
 
-        for i in np.random.choice(np.arange(size), np.random.randint(1, int(size/3))):
-            img[:, i] = 1
+        if center:
+            lower_bound = int(size/2) - 3
+            upper_bound = int(size/2) + 4
+        else:
+            lower_bound = 1
+            upper_bound = size - 1
+
+        for i in np.random.choice(np.arange(lower_bound, upper_bound), np.random.randint(1, min(int(size/4), upper_bound-lower_bound - 1))):
+            img[:, i] = np.True_
+        return img
+
+    @staticmethod
+    def Cross(size: int = 16, center=False):
+
+        img = np.zeros((size, size), dtype=np.bool)
+
+        if center:
+            lower_bound = int(size/2) - 1
+            upper_bound = int(size/2) + 2
+        else:
+            lower_bound = 5
+            upper_bound = size - 5
+
+        x = np.random.randint(lower_bound, upper_bound)
+
+        y = np.random.randint(x-2, x+3)
+
+        h_length = min(x - 1, size - x-1,
+                       np.random.random_integers(int(size/4), int(size/2)))
+        v_length = min(y - 1, size - y-1,
+                       np.random.random_integers(h_length-2, h_length+2))
+
+        img[x-h_length:x+h_length, y] = np.True_
+        img[x, y-v_length:y+v_length] = np.True_
         return img
 
     # Returns an image with added random noise with probability p
