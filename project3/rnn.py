62cd9d8b2e4be7e31a21b4bb6065f8d29495be0e
Jostein Tysse <josteiht@stud.ntnu.no>
2022-04-05 23:48:43 +0200

    Add rnn skeleton

---
+++ project3/rnn.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.callbacks import TensorBoard
+
+
+class RNN:
+
+    def __init__(self) -> None:
+        pass
+
+    def train(self) -> None:
+        pass
+
+    def predict(self, x):
+        pass
