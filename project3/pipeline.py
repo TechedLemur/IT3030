d18e86bbc0e97f1dd6725f5f06cfe358b994ac9b
Jostein Tysse <josteiht@stud.ntnu.no>
2022-04-22 00:23:51 +0200

    Feat early stage working model

--- project3/pipeline.py
+++ project3/pipeline.py
@@ -8,7 +8,7 @@ from sklearn import preprocessing
 class Pipeline:
 
     @staticmethod
-    def format_data(data:pd.DataFrame, k:int, features:list[str])->Tuple[np.ndarray, np.array]:
+    def format_data(data: pd.DataFrame, k: int, features: list[str], drop_prev_no=0) -> Tuple[np.ndarray, np.array]:
         '''
         input:
             data - the pandas dataframe of (n, p+1) shape, where n is the number of rows,
@@ -26,47 +26,62 @@ class Pipeline:
 
         X_data = np.zeros([x.shape[0]-k, k, x.shape[1]])
         y_data = []
-        
+
         # run loop to slice k-number of previous rows as 1 sequence to predict
         # 1 target and save them to X_data matrix and y_data list
         for i in range(k, x.shape[0]):
             cur_sequence = x[i-k: i]
             cur_target = y[i-1]
-            
-            X_data[i-k,:, :] = cur_sequence.reshape(1, k, X_data.shape[2])
+
+            if drop_prev_no > 0:
+                cur_sequence[-drop_prev_no:, -1] = 0
+
+            X_data[i-k, :, :] = cur_sequence.reshape(1, k, X_data.shape[2])
             y_data.append(cur_target)
-        
+
         return X_data, np.asarray(y_data)
 
     @staticmethod
-    def process(data:pd.DataFrame)->Tuple[pd.DataFrame, preprocessing.StandardScaler]:
+    def process(data: pd.DataFrame) -> Tuple[pd.DataFrame, preprocessing.StandardScaler]:
         """
         Process the dataframe. Remove outliers, scale etc.
         Returns the transformed dataframe along with a scaler to inverse scale the target.
         """
-        df =data.copy(deep=True)
+        df = data.copy(deep=True)
 
-        df.loc[df['y'] < -5000, 'y'] = 5.687162	# Set wrong values to mean
-        df.loc[df['y'] > 5000, 'y'] = 5.687162	# Set wrong values to mean
+        df['flow'] = -df['flow']  # Flip sign because of mistake in dataset
 
-        df = df.drop(columns='river') # Drop useless column
+        df.loc[df['y'] < -5000, 'y'] = 5.687162  # Set wrong values to mean
+        df.loc[df['y'] > 5000, 'y'] = 5.687162  # Set wrong values to mean
+        # TODO: CLip more values
+        df = df.drop(columns='river')  # Drop useless column
 
         numerical_features = ['hydro', 'micro', 'thermal', 'wind', 'total', 'y',
-       'sys_reg', 'flow']
+                              'sys_reg', 'flow']
 
         scaler = preprocessing.StandardScaler().fit(df[numerical_features])
 
         df[numerical_features] = scaler.transform(df[numerical_features])
 
-        dt = df.start_time.apply(lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S') )
-        df['time_of_day']= dt.apply(lambda x: x.hour)
-        df['time_of_week']= dt.apply(lambda x: x.weekday())
-        df['time_of_year']= dt.apply(lambda x: x.month % 12 // 3)
+        dt = df.start_time.apply(
+            lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
+        df['time_of_day'] = dt.apply(lambda x: x.hour)
+        df['time_of_week'] = dt.apply(lambda x: x.weekday())
+        df['time_of_year'] = dt.apply(lambda x: x.month % 12 // 3)
+        df['new_hour'] = dt.apply(lambda x: (x.minute == 0)).astype(np.float32)
 
-        last_day_offset = 24*60 // 5 # timesteps are 5 minutes
-        last_week_offset =7* 24*60 // 5 
+        df['time_of_day_sin'] = np.sin(df['time_of_day'] * (2 * np.pi / 23))
+        df['time_of_day_cos'] = np.cos(df['time_of_day'] * (2 * np.pi / 23))
+        df['time_of_week_sin'] = np.sin(df['time_of_week'] * (2 * np.pi / 6))
+        df['time_of_week_cos'] = np.cos(df['time_of_week'] * (2 * np.pi / 6))
+        df['time_of_year_sin'] = np.sin(df['time_of_year'] * (2 * np.pi / 11))
+        df['time_of_year_cos'] = np.cos(df['time_of_year'] * (2 * np.pi / 11))
+
+        last_day_offset = 24*60 // 5  # timesteps are 5 minutes
+        last_week_offset = 7 * 24*60 // 5
         df['previous_y'] = df['y'].shift(1)
         df['prev_day_y'] = df['y'].shift(last_day_offset)
         df['prev_week_y'] = df['y'].shift(last_week_offset)
-        df = df[last_week_offset:] # Remove the first rows which contains NaNs.
+        # Remove the first rows which contains NaNs.
+        df = df[last_week_offset:]
         return df, scaler
--- project3/rnn.py
+++ project3/rnn.py
@@ -1,17 +1,62 @@
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.python.keras import layers
-from tensorflow.python.keras.callbacks import TensorBoard
+from tensorflow.keras import layers
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 class RNN:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, k, no_features) -> None:
+        self.k = k
+        input_layer = keras.Input(shape=(k, no_features), name='Input layer')
 
-    def train(self) -> None:
-        pass
-        layers.LSTM()
+        x = layers.LSTM(50, return_sequences=True)(input_layer)
+        x = layers.Dropout(0.1)(x)
+        x = layers.LSTM(50)(x)
+        x = layers.Dropout(0.1)(x)
+        x = layers.Dense(100, activation='relu')(x)
+
+        output_layer = layers.Dense(1, activation='linear')(x)
+
+        model = keras.Model(input_layer, output_layer, name='LSTM_Model')
+
+        model.compile(
+            optimizer=keras.optimizers.Adam(learning_rate=0.0001),
+            loss=keras.losses.MeanSquaredError()
+        )
+        self.model = model
+
+    def train(self, x_train, y_train, x_valid, y_valid, epochs=10, batch_size=128) -> None:
+
+        self.history = self.model.fit(
+            x=x_train,
+            y=y_train,
+            epochs=epochs,
+            batch_size=batch_size,
+            validation_data=(x_valid, y_valid))
+
+    def forecast(self, x, n=20, s=0):
+
+        preds = np.zeros(s+n)
+        x_t = x.copy()
+        # model(x_test[:w])
+        for i in range(n):
+            p = self.model(x_t[s+i:s+i+1])[0][0]
+
+            for j in range(i, i+self.k):
+                # Replace "previous_y" for the next k rows
+                x_t[s+j+1][-(1+j-i)][-1] = p
+            preds[s+i] = p
+        return preds
 
     def predict(self, x):
+
+        return self.model.predict(x)
+
+    def plot_train_history(self):
+        pass
+
+    def forecast_n(self, x, n, plot=True):
         pass
