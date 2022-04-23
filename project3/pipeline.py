96b479b1156091816ddf0b684acb88a39c38b943
Jostein Tysse <josteiht@stud.ntnu.no>
2022-04-23 18:14:02 +0200

    Feat structural imbalance

--- project3/pipeline.py
+++ project3/pipeline.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from datetime import datetime
 from sklearn import preprocessing
+from scipy import interpolate
 
 
 class Pipeline:
@@ -29,12 +30,16 @@ class Pipeline:
 
         # run loop to slice k-number of previous rows as 1 sequence to predict
         # 1 target and save them to X_data matrix and y_data list
+
+        p = 0.05
         for i in range(k, x.shape[0]):
             cur_sequence = x[i-k: i]
             cur_target = y[i-1]
 
             if drop_prev_no > 0:
-                cur_sequence[-drop_prev_no:, -1] = 0
+                mask = np.random.choice(
+                    a=[0, 1], size=(drop_prev_no), p=[p, 1-p])
+                cur_sequence[-drop_prev_no:, -1] *= mask
 
             X_data[i-k, :, :] = cur_sequence.reshape(1, k, X_data.shape[2])
             y_data.append(cur_target)
@@ -42,7 +47,7 @@ class Pipeline:
         return X_data, np.asarray(y_data)
 
     @staticmethod
-    def process(data: pd.DataFrame) -> Tuple[pd.DataFrame, preprocessing.StandardScaler]:
+    def process(data: pd.DataFrame, altered=False) -> Tuple[pd.DataFrame, preprocessing.StandardScaler]:
         """
         Process the dataframe. Remove outliers, scale etc.
         Returns the transformed dataframe along with a scaler to inverse scale the target.
@@ -56,18 +61,15 @@ class Pipeline:
         # TODO: CLip more values
         df = df.drop(columns='river')  # Drop useless column
 
-        numerical_features = ['hydro', 'micro', 'thermal', 'wind', 'total', 'y',
-                              'sys_reg', 'flow']
-
-        scaler = preprocessing.StandardScaler().fit(df[numerical_features])
-
-        df[numerical_features] = scaler.transform(df[numerical_features])
-
+        # Time features
         dt = df.start_time.apply(
             lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'))
         df['time_of_day'] = dt.apply(lambda x: x.hour)
         df['time_of_week'] = dt.apply(lambda x: x.weekday())
         df['time_of_year'] = dt.apply(lambda x: x.month % 12 // 3)
+        # Divide the hour into 0,1,..11
+        df['time_of_hour'] = dt.apply(lambda x: (x.minute // 5))
+
         df['new_hour'] = dt.apply(lambda x: (x.minute == 0)).astype(np.float32)
 
         df['time_of_day_sin'] = np.sin(df['time_of_day'] * (2 * np.pi / 24))
@@ -76,12 +78,43 @@ class Pipeline:
         df['time_of_week_cos'] = np.cos(df['time_of_week'] * (2 * np.pi / 7))
         df['time_of_year_sin'] = np.sin(df['time_of_year'] * (2 * np.pi / 12))
         df['time_of_year_cos'] = np.cos(df['time_of_year'] * (2 * np.pi / 12))
+        df['time_of_hour_sin'] = np.sin(df['time_of_hour'] * (2 * np.pi / 12))
+        df['time_of_hour_cos'] = np.cos(df['time_of_hour'] * (2 * np.pi / 12))
 
+        df['sum'] = df['total'] + df['flow']
+
+        # Calculate structural imbalance
+        tdf = df.copy(deep=True)
+        n = len(tdf)
+        start = (6-tdf.time_of_hour[0]) % 12
+        x = np.arange(start, n, 12)
+        tck = interpolate.splrep(x, tdf['sum'][x], s=1)
+        xfit = np.arange(0, n)
+        yfit = interpolate.splev(xfit, tck, der=0)
+
+        tdf['smooth'] = yfit
+
+        df['structural_imbalance'] = tdf['sum'] - tdf['smooth']
+
+        if altered:  # Swap y variable for the altered forecasting task
+            df['y'] = df['y'] - df['structural_imbalance']
+
+        # Standard scale numerical features
+        numerical_features = ['hydro', 'micro', 'thermal', 'wind', 'total', 'y',
+                              'sys_reg', 'flow', 'structural_imbalance', 'sum']
+
+        scaler = preprocessing.StandardScaler().fit(df[numerical_features])
+
+        df[numerical_features] = scaler.transform(df[numerical_features])
+
+        # Lag features
         last_day_offset = 24*60 // 5  # timesteps are 5 minutes
         last_week_offset = 7 * 24*60 // 5
         df['previous_y'] = df['y'].shift(1)
+        df['previous_20y'] = df['y'].shift(20)
         df['prev_day_y'] = df['y'].shift(last_day_offset)
         df['prev_week_y'] = df['y'].shift(last_week_offset)
+
         # Remove the first rows which contains NaNs.
         df = df[last_week_offset:]
         return df, scaler
--- project3/rnn.py
+++ project3/rnn.py
@@ -1,32 +1,48 @@
+from sklearn import metrics
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from tensorflow.keras import backend as K
+from tensorflow.keras import regularizers
 
 
 class RNN:
 
     def __init__(self, k, no_features) -> None:
         self.k = k
-        input_layer = keras.Input(shape=(k, no_features), name='Input layer')
+        input_layer = keras.Input(shape=(k, no_features))
 
-        x = layers.LSTM(50, return_sequences=True)(input_layer)
-        x = layers.Dropout(0.1)(x)
-        x = layers.LSTM(50)(x)
-        x = layers.Dropout(0.1)(x)
-        x = layers.Dense(100, activation='relu')(x)
+        x = layers.LSTM(30, return_sequences=True, kernel_regularizer=regularizers.L1L2(l1=1e-5, l2=1e-4),
+                        bias_regularizer=regularizers.L2(1e-4),
+                        activity_regularizer=regularizers.L2(1e-5))(input_layer)
+        x = layers.Dropout(0.13)(x)
+        x = layers.LSTM(20, kernel_regularizer=regularizers.L1L2(l1=1e-5, l2=1e-4),
+                        bias_regularizer=regularizers.L2(1e-4),
+                        activity_regularizer=regularizers.L2(1e-5))(x)
+        x = layers.Dropout(0.12)(x)
+        x = layers.Dense(100, activation='relu', kernel_regularizer=regularizers.L1L2(l1=1e-5, l2=1e-4),
+                         bias_regularizer=regularizers.L2(1e-4),
+                         activity_regularizer=regularizers.L2(1e-5))(x)
 
         output_layer = layers.Dense(1, activation='linear')(x)
 
         model = keras.Model(input_layer, output_layer, name='LSTM_Model')
 
         model.compile(
-            optimizer=keras.optimizers.Adam(learning_rate=0.0001),
-            loss=keras.losses.MeanSquaredError()
+            optimizer=keras.optimizers.Adam(learning_rate=0.00012),
+            # loss=keras.losses.MeanSquaredError()
+            loss=RNN.root_mean_squared_error
+
+
         )
         self.model = model
+
+    @staticmethod
+    def root_mean_squared_error(y_true, y_pred):
+        return K.sqrt(K.mean(K.square(y_pred - y_true)))
 
     def train(self, x_train, y_train, x_valid, y_valid, epochs=10, batch_size=128) -> None:
 
@@ -37,17 +53,17 @@ class RNN:
             batch_size=batch_size,
             validation_data=(x_valid, y_valid))
 
-    def forecast(self, x, n=20, s=0):
+    def forecast(self, x, n=20, s=0, replace=True):
 
         preds = np.zeros(s+n)
         x_t = x.copy()
         # model(x_test[:w])
         for i in range(n):
             p = self.model(x_t[s+i:s+i+1])[0][0]
-
-            for j in range(i, i+self.k):
-                # Replace "previous_y" for the next k rows
-                x_t[s+j+1][-(1+j-i)][-1] = p
+            if replace:
+                for j in range(i, min(i+self.k, i+n)):
+                    # Replace "previous_y" for the next rows
+                    x_t[s+j+1][-(1+j-i)][-1] = p
             preds[s+i] = p
         return preds
 
@@ -56,7 +72,53 @@ class RNN:
         return self.model.predict(x)
 
     def plot_train_history(self):
-        pass
+        plt.plot(self.history.history['loss'])
+        plt.plot(self.history.history['val_loss'])
+        plt.title('model loss')
+        plt.ylabel('loss')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'valid'], loc='upper right')
+        plt.savefig("./figures/training_loss.png")
+        plt.show()
 
-    def forecast_n(self, x, n, plot=True):
-        pass
+    def forecast_15(self, x, y, M=100, n=20, replace=True, path=""):
+        plt.figure(figsize=(20, 20))
+        for i in range(15):
+            s = np.random.randint(M, len(x)-M)
+            p = self.forecast(x, n=n, s=s, replace=replace)
+
+            t = np.arange(len(x))
+
+            plt.subplot(3, 5, i+1)
+
+            plt.plot(t[s-M:s+1], y[s-M:s+1])
+            plt.plot(t[s:s+n], p[s:s+n])
+            plt.plot(t[s:s+n], y[s:s+n])
+            plt.legend(['y_historical', 'y_pred', 'y_target'],
+                       loc='upper left')
+        plt.savefig(path)
+        plt.show()
+
+    def forecast_1(self, x, y, M=100, n=20, s=None, replace=True, path=""):
+        if not s:
+            s = np.random.randint(M, len(x)-M)
+        p = self.forecast(x, n=n, s=s, replace=replace)
+
+        t = np.arange(len(x))
+
+        plt.plot(t[s-M:s+1], y[s-M:s+1])
+        plt.plot(t[s:s+n], p[s:s+n])
+        plt.plot(t[s:s+n], y[s:s+n])
+        plt.legend(['y_historical', 'y_pred', 'y_target'],
+                   loc='upper left')
+        plt.savefig(path)
+        plt.show()
+
+    def save_model(self, path):
+        self.model.save(path)
+
+    def load_model(self, path, new_k=None):
+        self.model = keras.models.load_model(
+            path, custom_objects={'root_mean_squared_error': RNN.root_mean_squared_error})
+        if new_k:
+            self.k = new_k
